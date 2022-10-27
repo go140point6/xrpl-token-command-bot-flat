0d3b9acd89456640b3b0881dbc22619a365c6180
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-10-27 09:39:38 -0700

    api

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const beep = {
 
 async function getPrices() {
     await axios.get(`https://api.onthedex.live/public/v1/ticker/CSC.rCSCManTZ8ME9EoLrSHHYKW8PPwWMgkwr:XRP`).then(res => {
-        console.log(res.data.pairs[].last);
+        console.log(res.data.pairs);
     })
 };
 
