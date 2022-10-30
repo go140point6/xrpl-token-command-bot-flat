bb0c7c7804c522b304e374707441af4143734fb2
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-10-30 15:41:50 -0700

    f

--- pairs.js
+++ pairs.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 async function getPairs() {
     await axios.get(`https://api.onthedex.live/public/v1/daily/tokens`).then(res => {
         this.data = res.data;
+        Array.isArray(this.data);
         //console.log(this.data);
 
         Array.from(res).forEach(item => console.log(item));
