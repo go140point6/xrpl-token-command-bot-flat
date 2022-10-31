ec079352e0470079713a4de958bd583027916e56
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-10-30 20:18:27 -0700

    d

--- pairs.js
+++ pairs.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database('./data/tokens.db', (err) => {
         //createDatabase();
-        
+        getPairs();
 });
 
 function createDatabase() {
