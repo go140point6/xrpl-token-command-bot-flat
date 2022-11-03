edc52977527ed6fbf8edff26ff4d77f5234fdaf7
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-03 14:06:32 -0700

    d

--- betterdb.js
+++ betterdb.js
@@ -9,14 +9,53 @@ const Database = require('better-sqlite3');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const db = new Database('./data/tokens.db', { verbose: console.log });
+const tableName = "tokens";
+const db = new Database('./data/tokens.db', (err) => {
+    createDatabase();
+    getTokens();
+    getMoreTokens();
+});
 
-var tableName = "tokens";
-var fields = "(id INT PRIMARY KEY NOT NULL, currency TEXT, issuer TEXT)";
-var SQLquery = `CREATE TABLE IF NOT EXISTS  ${tableName} ${fields}`;
-console.log(SQLquery);
-const createTable = db.prepare(SQLquery);
-createTable.run();
+function createDatabase() {
+    let fields = "(id INT PRIMARY KEY NOT NULL, currency TEXT, issuer TEXT)";
+    let sql = `CREATE TABLE IF NOT EXISTS  ${tableName} ${fields}`;
+    let createTable = db.prepare(sql);
+    createTable.run();
+};
+
+async function getTokens() {
+    let sql = `SELECT EXISTS (SELECT 1 FROM ${tableName}`;
+    console.log(sql);
+    /*
+    await axios.get(`https://api.onthedex.live/public/v1/aggregator`).then(res => {
+        //console.log(res.data);
+        //console.log(res.data.tokens);
+        //console.log(res.data.tokens[0].currency);
+        let count = 0;
+        let id = 0;
+        const allTokens = res.data.tokens.forEach((element) => {
+            count++;
+            id++;
+            var sql = "INSERT INTO tokens(id,issuer,currency) VALUES(?,?,?)";
+            //console.log(sql);
+            var params = [id, element.issuer, element.currency];
+            //console.log(params);
+            db.run(sql, params, function(err) {
+                //console.log(element.issuer);
+                if (err) {
+                    console.log("Error when adding token: ", err.message);
+                }
+                //console.log(`inserted: ${this.lastID}`);
+                console.log(`${id},${element.issuer},${element.currency}`);
+            });
+            //console.log(element.currency + " and " + element.issuer);
+            //count++;
+        })
+        console.log(count);
+        //let length = allTokens.length;
+        //console.log(length);
+    });*/
+}
 
 var inUSD = 0;
 var currency;
