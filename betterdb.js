b04bfc55374b5e656d01fdae0ba9f8347ca95fb5
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-03 14:39:21 -0700

    d

--- betterdb.js
+++ betterdb.js
@@ -88,6 +88,7 @@ client.once(Events.ClientReady, c => {
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
+    await interaction.deferReply();
 	const { commandName } = interaction;
 
 	if (commandName === 'ping') {
