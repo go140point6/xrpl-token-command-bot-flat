126b7b536f97289eb758408650c9cd3dc12770eb
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-04 15:35:09 -0700

    d

--- createStuff.js
+++ createStuff.js
@@ -4,21 +4,34 @@ const Database = require('better-sqlite3');
 const db = new Database('./data/tokens.db', {verbose: console.log });
 
 var tableName = "tokens";
-var fields = "(id PRIMARY KEY, currency TEXT, issuer TEXT)";
-var sql1 = `CREATE TABLE IF NOT EXISTS ${tableName} ${fields}`;
-const createTable = db.prepare(sql1);
+var fields = "(currency TEXT, issuer TEXT)";
+var sql = `CREATE TABLE IF NOT EXISTS ${tableName} ${fields}`;
+const createTable = db.prepare(sql);
 createTable.run();
 
 axios.get(`https://api.onthedex.live/public/v1/aggregator`).then(res => {
-    console.log(res.data.tokens);
+    //console.log(res.data.tokens);
+    let count = 0;
+    let id = 0;
+    const insert = db.prepare('INSERT INTO tokens (currency, issuer) VALUES (@currency, @issuer)');
+    //var insertQuery = "INSERT INTO tokens VALUES (?,?,?)";
+
+    const insertMany = db.transaction((tokens) => {
+        for (const token of tokens) insert.run(token)
+    })
+
+    insertMany(res.data.tokens);
+    //db.prepare(insertQuery).run(id, currency, issuer);
+    
+    //const stmt = db.prepare("SELECT * FROM tokens");
+    //var results = stmt.all();
+    //console.log(results);
+
 });
-/*
-var insertQuery = "INSERT INTO tokens VALUES (?,?,?)";
-db.prepare(insertQuery).run(id, currency, issuer);
+
 const stmt = db.prepare("SELECT * FROM tokens");
 var results = stmt.all();
 console.log(results);
-*/
 
 /*
 async function getTokens() {
