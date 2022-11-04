acc2754b8c35989424e75ad6152a44ce46ee1689
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-04 11:14:45 -0700

    d

--- pairs.js
+++ pairs.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database('./data/tokens.db', (err) => {
-        createDatabase();
-        //getPairs();
+        //createDatabase();
+        getPairs();
 });
 
 function createDatabase() {
@@ -33,32 +33,32 @@ function createTables(newdb) {
 async function getPairs() {
     //await axios.get(`https://api.onthedex.live/public/v1/daily/tokens?by=trades`).then(res => {
     await axios.get(`https://api.onthedex.live/public/v1/aggregator`).then(res => {
-        //console.log(res.data);
-        //console.log(res.data.tokens);
-        //console.log(res.data.tokens[0].currency);
+        console.log(res.data);
+        console.log(res.data.tokens);
+        console.log(res.data.tokens[0].currency);
         let count = 0;
         let id = 0;
         const allTokens = res.data.tokens.forEach((element) => {
             count++;
             id++;
             var sql = "INSERT INTO tokens(id,issuer,currency) VALUES(?,?,?)";
-            //console.log(sql);
+            console.log(sql);
             var params = [id, element.issuer, element.currency];
-            //console.log(params);
+            console.log(params);
             db.run(sql, params, function(err) {
-                //console.log(element.issuer);
+                console.log(element.issuer);
                 if (err) {
                     console.log("Error when adding token: ", err.message);
                 }
-                //console.log(`inserted: ${this.lastID}`);
+                console.log(`inserted: ${this.lastID}`);
                 console.log(`${id},${element.issuer},${element.currency}`);
             });
-            //console.log(element.currency + " and " + element.issuer);
-            //count++;
+            console.log(element.currency + " and " + element.issuer);
+            count++;
         })
         console.log(count);
-        //let length = allTokens.length;
-        //console.log(length);
+        let length = allTokens.length;
+        console.log(length);
     });
 }
 
